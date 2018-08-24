65e90845ad7c18d87be44b1c7fdcc9341f656ecf
Prof. Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-08-24 12:05:13 -0300

    Commit Inicial

---
+++ app/src/main/java/br/pro/adalto/appcompras_m171/FormularioActivity.java
@@ -0,0 +1,13 @@
+package br.pro.adalto.appcompras_m171;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+    }
+}
