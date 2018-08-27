e41f6f2388fb43e8ccb4bd119e19d7aad9225852
Prof. Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-08-27 12:04:24 -0300

    Finalizada a implementação das telas e criada a classe Categoria

--- app/src/main/java/br/pro/adalto/appcompras_m171/FormularioActivity.java
+++ app/src/main/java/br/pro/adalto/appcompras_m171/FormularioActivity.java
@@ -1,7 +1,12 @@
 package br.pro.adalto.appcompras_m171;
 
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -10,4 +15,44 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_formulario, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if ( item.getItemId() == R.id.menu_nova_categoria ){
+            cadastrarCategoria();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void cadastrarCategoria(){
+        AlertDialog.Builder alerta = new AlertDialog.Builder(this);
+        alerta.setTitle( getResources().getString(R.string.txtNovaCategoria) );
+        alerta.setIcon( android.R.drawable.ic_menu_edit );
+
+        EditText etNomeCategoria = new EditText(this);
+        etNomeCategoria.setHint( R.string.hintCategoria );
+        etNomeCategoria.setTextColor(Color.BLUE);
+
+        alerta.setView( etNomeCategoria );
+
+        alerta.setNeutralButton(
+                getResources().getString(R.string.txtCancelar) ,
+                null );
+        alerta.setPositiveButton(
+                getResources().getString(R.string.txtSalvar) ,
+                null);
+        alerta.show();
+
+    }
+
+
 }
---
+++ app/src/main/java/br/pro/adalto/appcompras_m171/model/Categoria.java
@@ -0,0 +1,35 @@
+package br.pro.adalto.appcompras_m171.model;
+
+/**
+ * Created by assparremberger on 27/08/2018.
+ */
+
+public class Categoria {
+
+    private int id;
+    private String nome;
+
+    public Categoria(){
+
+    }
+
+    public Categoria(String nome){
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
